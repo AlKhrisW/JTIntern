6d08155ec7e1711b4afa0c816222feec7f71ed5c
Aldo Khrisna <aldokhrisna@gmail.com>
2026-05-23 10:23:40 +0700

    feat: kerangka siakad dummy

---
+++ dummy-siakad/app/Http/Controllers/MataKuliahModelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MataKuliahModel;
+use Illuminate\Http\Request;
+
+class MataKuliahModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MataKuliahModel $mataKuliahModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MataKuliahModel $mataKuliahModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MataKuliahModel $mataKuliahModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MataKuliahModel $mataKuliahModel)
+    {
+        //
+    }
+}
---
+++ dummy-siakad/app/Http/Controllers/NilaiModelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NilaiModel;
+use Illuminate\Http\Request;
+
+class NilaiModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NilaiModel $nilaiModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NilaiModel $nilaiModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, NilaiModel $nilaiModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NilaiModel $nilaiModel)
+    {
+        //
+    }
+}
