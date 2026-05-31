573843ce419e6e5c19bff8a388edf807f0c23f14
Aldo Khrisna <aldokhrisna@gmail.com>
2026-05-31 20:29:51 +0700

    feat: api siakad

---
+++ dummy-siakad/app/Http/Resources/MahasiswaResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MahasiswaResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'nim' => $this['nim'],
+            'nama' => $this['nama'],
+            'email' => $this['email'],
+            'program_studi' => $this['program_studi'],
+            'ipk' => $this['ipk'],
+            'keahlian' => $this['keahlian'],
+        ];
+    }
+}
